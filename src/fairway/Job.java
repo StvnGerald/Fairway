5205803a8bbe4ecdd4b7bb822103b8936fa11e95
StvnGerald <stevengerald9924@gmail.com>
2024-12-30 11:05:21 +0700

    Update Job.java

--- src/fairway/Job.java
+++ src/fairway/Job.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package fairway;
+import java.util.ArrayList;
 
 /**
  *
@@ -19,7 +20,9 @@ public class Job {
     private String syaratPelamar;
     private String expireDate;
     private int limitPenerimaan;
-
+    //
+    private ArrayList<Jobseeker> applicants = new ArrayList<>();
+    //
     public Job(String title, String kategori, String description, String companyName, String lokasiKerja, String syaratPelamar, String hireDate, String gaji, String expireDate, int limitPenerimaan) {
         this.title = title;
         this.description = description;
@@ -72,6 +75,50 @@ public class Job {
     public String getExpireDate() {
         return expireDate;
     }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public void setKategori(String kategori) {
+        this.kategori = kategori;
+    }
+
+    public void setLokasiKerja(String lokasiKerja) {
+        this.lokasiKerja = lokasiKerja;
+    }
+
+    public void setHireDate(String hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public void setGaji(String gaji) {
+        this.gaji = gaji;
+    }
+
+    public void setSyaratPelamar(String syaratPelamar) {
+        this.syaratPelamar = syaratPelamar;
+    }
+
+    public void setExpireDate(String expireDate) {
+        this.expireDate = expireDate;
+    }
+
+    public void setLimitPenerimaan(int limitPenerimaan) {
+        this.limitPenerimaan = limitPenerimaan;
+    }
+
+    public void setApplicants(ArrayList<Jobseeker> applicants) {
+        this.applicants = applicants;
+    }
     
     public void displayJobDetails() {
         System.out.println("\n=== Detail Lowongan ===");
@@ -82,9 +129,28 @@ public class Job {
         System.out.println("Lokasi Kerja      : " + lokasiKerja);
         System.out.println("Tanggal Mulai     : " + hireDate);
         System.out.println("Gaji              : " + gaji);
-        System.out.println("Syarat Pelamar    : " + syaratPelamar);
+        System.out.println("Syarat Pelamar    : " + syaratPelamar);       
         System.out.println("Batas Pendaftaran : " + expireDate);
         System.out.println("Kuota Penerimaan  : " + limitPenerimaan);
     }
-    
+    //
+    public ArrayList<Jobseeker> getApplicants() {
+        return applicants;
+    }
+
+    public void addApplicant(Jobseeker jobseeker) {
+        applicants.add(jobseeker);
+    }
+
+    public void displayApplicants() {
+        if (applicants.isEmpty()) {
+            System.out.println("Belum ada pelamar.");
+        } else {
+            System.out.println("\n=== Daftar Pelamar ===");
+            for (int i = 0; i < applicants.size(); i++) {
+                System.out.println((i + 1) + ". " + applicants.get(i).getUsername());
+            }
+        }
+    }
+    //
 }
